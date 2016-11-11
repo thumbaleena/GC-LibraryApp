a314d5185f3340b192131be6e77cd95397da740e
hebruce259 <saundrabruce@hotmail.com>
2016-11-11 12:40:33 -0500

    Add User info in Class

---
+++ LibraryApp/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    class User
+    {
+        private string email;
+        private string lastName;
+        private string firstName;
+        private bool powerUser;
+
+        public User(string email, string lastName, string firstName, bool powerUser)
+        {
+            this.email = email;
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.powerUser = powerUser;
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+        }
+
+        public bool PowerUser
+        {
+            get
+            {
+                return powerUser;
+            }
+
+        }
+    }
+}
---
+++ LibraryApp/book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    class book
+    {
+        private string title;
+        private string author;
+        private string status;
+        private DateTime dueDate;
+    }
+}
